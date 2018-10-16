47550991288aaf37360c251785c136e6a27665c1
Peter Konradi <codingchipmunk@gmail.com>
2018-10-16 11:55:31 +0200

    Rewrote MarshalText for MBean
    Reduced duplicate code and configurability of MBean text marshaling by introducing a new function writeAttribute which will write attribute names and its value into an byte buffer

--- mBean.go
+++ mBean.go
@@ -11,30 +11,48 @@ type MBean struct {
 	Name    string `json:"name,omitempty"`
 }
 
+const Context_identifier = "context"
+const Type_identifier = "type"
+const Name_identifier = "name"
+const Domain_seperator = ':'
+const Attribute_seperator = ','
+const Attribute_assignment = '='
 
+//	Marsahlls an MBean into a form of Domain:context=bean.context,type=bean.type,name=bean.name
+//	Empty fields will be ignored when marshalled
 func (bean *MBean) MarshalText() ([]byte, error) {
 	var buff bytes.Buffer
+
+	// Omit writing a domain if its empty
 	if bean.Domain != "" {
 		buff.WriteString(bean.Domain)
-		buff.WriteByte(':')
+		buff.WriteByte(Domain_seperator)
 	}
+
+	// Omit empty attributes
 	if bean.Context != "" {
-		buff.WriteString("context=")
-		buff.WriteString(bean.Context)
-		buff.WriteByte(',')
+		writeAttribute(&buff, Context_identifier, bean.Context)
 	}
 	if bean.Type != "" {
-		buff.WriteString("type=")
-		buff.WriteString(bean.Type)
-		buff.WriteByte(',')
+		writeAttribute(&buff, Type_identifier, bean.Type)
 	}
 	if bean.Name != "" {
-		buff.WriteString("name=")
-		buff.WriteString(bean.Name)
-		buff.WriteByte(',')
+		writeAttribute(&buff, Name_identifier, bean.Name)
 	}
+
+	// if the buffer is empty, return it without modifications
 	if buff.Len() == 0 {
 		return buff.Bytes(), nil
 	}
+
+	// return the bytes in the buffer without the trailing seperator
 	return buff.Bytes()[:buff.Len()-1], nil
+}
+
+//	Calls a set of functions to write the attribute name and its value into the buffer
+func writeAttribute(buff *bytes.Buffer, attribute_name string, attribute_value string) {
+	buff.WriteString(attribute_name)
+	buff.WriteByte(Attribute_assignment)
+	buff.WriteString(attribute_value)
+	buff.WriteByte(Attribute_seperator)
 }
