31f2eacf3747c5c6628aa584eff9085e7f33dc84
Peter Konradi <codingchipmunk@gmail.com>
2018-11-01 13:05:35 +0100

    Introduced backend package
    backend package holds functions needed to make requests to a Jolokia agent. May be used by clients

---
+++ backend/backend.go
@@ -0,0 +1,66 @@
+package backend
+
+import (
+	"bytes"
+	"encoding/json"
+	"io"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/codingchipmunk/jolokiago/messages"
+)
+
+// MakePOSTRequest makes an POST request to the Jolokia agent using the http.Client given to the client struct
+func  MakePOSTRequest(url string, client *http.Client, request messages.POSTRequest ) (resp messages.ResponseRoot, err error) {
+	// Marshal the request
+	body, err := request.POSTBody()
+	if err != nil {
+		return
+	}
+
+	// Use the http client to make the request
+	httpResp, err := client.Post(url, request.GetContentType(), bytes.NewReader(body))
+	if err != nil {
+		return
+	}
+	// Immediately defer Body.Close() (idiomatic)
+	defer httpResp.Body.Close()
+
+	return unmarshalResponse(httpResp.Body)
+}
+
+// MakeGETRequest makes an GET request to the Jolokia agent using the http.Client given to the client struct
+func MakeGETRequest(url string, client *http.Client,request messages.GETRequest) (resp messages.ResponseRoot, err error) {
+	// Create a new Buffer for the url and the get-params
+	urlBuff := bytes.Buffer{}
+	urlBuff.WriteString(url)
+	bts, err := request.GetAppendix()
+	if err != nil {
+		return
+	}
+	urlBuff.Write(bts)
+	// Use the http client to make the request
+	httpResp, err := client.Get(urlBuff.String())
+	if err != nil {
+		return
+	}
+	// Immediately defer Body.Close() (idiomatic)
+	defer httpResp.Body.Close()
+
+	return unmarshalResponse(httpResp.Body)
+
+}
+
+// unmarshalResponse unmarshals the response from a response body (or any struct implementing the io.ReadCloser interface)
+func unmarshalResponse(responseBody io.ReadCloser) (resp messages.ResponseRoot, err error) {
+	// Read the response body
+	httpBody, err := ioutil.ReadAll(responseBody)
+	if err != nil {
+		return
+	}
+
+	// Unmarshal the response body into the response.ResponseRoot struct
+	err = json.Unmarshal(httpBody, &resp)
+
+	return
+}
